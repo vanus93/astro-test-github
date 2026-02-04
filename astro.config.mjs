f44b31c08b4dec8fb3a863284e64fed185dab7ac
vanus93 <salvanus93@gmail.com>
2026-02-04 11:49:27 +0700

    Initial Astro test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://vanus93.github.io',
+  base: '/astro-test-github/',
+  output: 'static'
+});
